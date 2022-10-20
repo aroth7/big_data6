e22095b0076e481d8ba60fd14abd8b4c76586aa6
cchen120 <catherine_chen@brown.edu>
2022-10-19 22:28:52 -0400

    cal

--- preprocess.py
+++ preprocess.py
@@ -1,6 +1,13 @@
 #%%
 import pandas as pd
 import numpy as np
+import pycountry_convert as pc
+
+def country_to_continent(country_name):
+    country_alpha2 = pc.country_name_to_country_alpha2(country_name)
+    country_continent_code = pc.country_alpha2_to_continent_code(country_alpha2)
+    country_continent_name = pc.convert_continent_code_to_continent_name(country_continent_code)
+    return country_continent_name
 
 # %%
 f = open('loans_ridge.csv', 'r', encoding="utf8")
@@ -33,8 +40,8 @@ data['positive_sentiment'] = data[pos_words].sum(axis=1)
 data['overall_sentiment'] = (data['positive_sentiment'] > data['negative_sentiment']) * 1 # cast to int
 
 # add indicators for if description is especially pos or especially neg
-data['high_negative_sentiment'] = data['negative_sentiment'].apply(lambda x : 1 if x > 4 else 0)
-data['high_positive_sentiment'] = data['positive_sentiment'].apply(lambda x : 1 if x > 4  else 0)
+# data['high_negative_sentiment'] = data['negative_sentiment'].apply(lambda x : 1 if x > 4 else 0)
+# data['high_positive_sentiment'] = data['positive_sentiment'].apply(lambda x : 1 if x > 4  else 0)
 
 # %% ADD 9 FACTOR VARIABLES (for gender, country, sector)
 
@@ -51,16 +58,74 @@ data = data.assign(is_female= lambda row: 1 if row.borrowers_borrower_gender is 
 data =  data.assign(is_bulgaria= lambda row: 1 if row.location_country is 'Bulgaria' else 0,
            is_iraq= lambda row: 1 if row.location_country is 'Iraq' else 0)
 
-# dummies for country = zambia and country = thailand becauuse those 2 have the lowest avg days_until_funded
+# dummies for country = zambia and country = thailand because those 2 have the lowest avg days_until_funded
 data =  data.assign(is_zambia= lambda row: 1 if row.location_country is 'Zambia' else 0,
            is_thailand= lambda row: 1 if row.location_country is 'Thailand' else 0)
 
-# dummies for sectors with avg days_until_funded < 1 (health, education, and arts) as well
+# dummies for sectors with avg days_until_funded < 1 (health, education, arts, agriculture, and food) as well
 # as sector with highest avg days_until_funded (housing)
 data =  data.assign(is_health=lambda row: 1 if row.sector is 'Health' else 0,
            is_ed=lambda row: 1 if row.sector is 'Education' else 0,
            is_arts=lambda row: 1 if row.sector is 'Arts' else 0,
+           is_agriculture=lambda row: 1 if row.sector is 'Agriculture' else 0,
+           is_food=lambda row: 1 if row.sector is 'Food' else 0,
            is_housing=lambda row: 1 if row.sector is 'Housing' else 0)
+
+    
+for index, row in data.iterrows():
+    country = row['country']
+    country_africa = ['congo', 'cote']
+    country_asia = ['timor', 'myanmar', 'lao']
+    if any(substring in country.lower() for substring in country_africa):
+        data.at[index, 'african'] = 1
+        data.at[index, 'asia'] = 0
+        data.at[index, 'north_american'] = 0
+        data.at[index, 'south_american'] = 0
+        data.at[index, 'european'] = 0
+    elif any(substring in country.lower() for substring in country_asia):
+        data.at[index, 'african'] = 0
+        data.at[index, 'asia'] = 1
+        data.at[index, 'north_american'] = 0
+        data.at[index, 'south_american'] = 0
+        data.at[index, 'european'] = 0
+    else: 
+        continent = country_to_continent(country)
+        if continent == 'Africa':
+            data.at[index, 'african'] = 1
+            data.at[index, 'asia'] = 0
+            data.at[index, 'north_american'] = 0
+            data.at[index, 'south_american'] = 0
+            data.at[index, 'european'] = 0
+        elif continent == 'Asia':
+            data.at[index, 'african'] = 0
+            data.at[index, 'asia'] = 1
+            data.at[index, 'north_american'] = 0
+            data.at[index, 'south_american'] = 0
+            data.at[index, 'european'] = 0
+        elif continent == 'North America':
+            data.at[index, 'african'] = 0
+            data.at[index, 'asia'] = 0
+            data.at[index, 'north_american'] = 1
+            data.at[index, 'south_american'] = 0
+            data.at[index, 'european'] = 0
+        elif continent == 'South America':
+            data.at[index, 'african'] = 0
+            data.at[index, 'asia'] = 0
+            data.at[index, 'north_american'] = 0
+            data.at[index, 'south_american'] = 1
+            data.at[index, 'european'] = 0
+        elif continent == 'Europe':
+            data.at[index, 'african'] = 0
+            data.at[index, 'asia'] = 0
+            data.at[index, 'north_american'] = 0
+            data.at[index, 'south_american'] = 0
+            data.at[index, 'european'] = 1
+        else: 
+            data.at[index, 'african'] = 0
+            data.at[index, 'asia'] = 0
+            data.at[index, 'north_american'] = 0
+            data.at[index, 'south_american'] = 0
+            data.at[index, 'european'] = 0
 
 # create dummies for different statuses (paid, refunded, defaulted, in_repayment)
 data =  data.assign(paid=lambda row: 1 if row.status is 'paid' else 0,
